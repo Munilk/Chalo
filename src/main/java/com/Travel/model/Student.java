31ffb6fca37b4f91cb82155953e27a328858bbba
Munilk <munilk631@gmail.com>
2025-08-03 13:07:56 +0530

    Student file created

--- src/main/java/com/Travel/model/Student.java
+++ src/main/java/com/Travel/model/Student.java
@@ -1,1 +1,10 @@
 //hi hello 
+import java.util.*;
+
+class Student{
+private int id,age;
+private String name;
+public Student(int id,String name,int age){
+this.id=id;this.name=name;this.age=age;
+}
+}
