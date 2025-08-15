fb17100521e96cf7e0719debab8955eb46233887
Munilk <77898051+Munilk@users.noreply.github.com>
2025-08-15 18:11:37 +0530

    Update controller.java

--- src/main/java/com/Travel/controller/controller.java
+++ src/main/java/com/Travel/controller/controller.java
@@ -1,1 +1,15 @@
+
+
 //
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+// Also import your own classes:
+import com.Travel.service.service // adjust the package if needed
+import com.Travel.model.product;    // adjust the package if needed
